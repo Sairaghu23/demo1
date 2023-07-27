ed697693cf30b0b23c85e7729c884b9c9db0d981
Sairaghu23 <131390376+Sairaghu23@users.noreply.github.com>
2023-07-27 22:42:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,113 +13,220 @@ function openFile(event) {
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0001.jpg" width="100%" alt="Result Image">';
         /*download.innerHTML = '<input type="button" value="Download" onclick="downloadFile(\'Dipoma1stresultspdf_page-0001.jpg\')">';*/
         scroll.innerHTML='<marquee><span id="color">For Dowload</span>:long press on the Result</marquee>';
+        var loginForm = document.getElementById('form');
+      loginForm.parentNode.removeChild(loginForm);
         break;
         case "22280-CM-002":
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0002.jpg" width="100%" alt="Result Image">';
+        scroll.innerHTML='<marquee><span id="color">For Dowload</span>:long press on the Result</marquee>';
+        var loginForm = document.getElementById('form');
+      loginForm.parentNode.removeChild(loginForm);
         break;
         case "22280-CM-003":
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0003.jpg" width="100%" alt="Result Image">';
+        scroll.innerHTML='<marquee><span id="color">For Dowload</span>:long press on the Result</marquee>';
+        var loginForm = document.getElementById('form');
+      loginForm.parentNode.removeChild(loginForm);
         break;
        case "22280-CM-004":
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0004.jpg" width="100%" alt="Result Image">';
+        scroll.innerHTML='<marquee><span id="color">For Dowload</span>:long press on the Result</marquee>';
+        var loginForm = document.getElementById('form');
+      loginForm.parentNode.removeChild(loginForm);
         break;
         case "22280-CM-005":
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0005.jpg" width="100%" alt="Result Image">';
+        scroll.innerHTML='<marquee><span id="color">For Dowload</span>:long press on the Result</marquee>';
+        var loginForm = document.getElementById('form');
+      loginForm.parentNode.removeChild(loginForm);
         break;
         case "22280-CM-006":
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0006.jpg" width="100%" alt="Result Image">';
+        scroll.innerHTML='<marquee><span id="color">For Dowload</span>:long press on the Result</marquee>';
+        var loginForm = document.getElementById('form');
+      loginForm.parentNode.removeChild(loginForm);
         break;
         case "22280-CM-007":
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0007.jpg" width="100%" alt="Result Image">';
+        scroll.innerHTML='<marquee><span id="color">For Dowload</span>:long press on the Result</marquee>';
+        var loginForm = document.getElementById('form');
+      loginForm.parentNode.removeChild(loginForm);
         break;
         case "22280-CM-008":
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0008.jpg" width="100%" alt="Result Image">';
+        scroll.innerHTML='<marquee><span id="color">For Dowload</span>:long press on the Result</marquee>';
+        var loginForm = document.getElementById('form');
+      loginForm.parentNode.removeChild(loginForm);
         break;
         case "22280-CM-009":
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0009.jpg" width="100%" alt="Result Image">';
+        scroll.innerHTML='<marquee><span id="color">For Dowload</span>:long press on the Result</marquee>';
+        var loginForm = document.getElementById('form');
+      loginForm.parentNode.removeChild(loginForm);
         break;
         case "22280-CM-010":
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0010.jpg" width="100%" alt="Result Image">';
+        scroll.innerHTML='<marquee><span id="color">For Dowload</span>:long press on the Result</marquee>';
+        var loginForm = document.getElementById('form');
+      loginForm.parentNode.removeChild(loginForm);
         break;
         case "22280-CM-011":
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0011.jpg" width="100%" alt="Result Image">';
+        scroll.innerHTML='<marquee><span id="color">For Dowload</span>:long press on the Result</marquee>';
+        var loginForm = document.getElementById('form');
+      loginForm.parentNode.removeChild(loginForm);
         break;
         case "22280-CM-012":
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0012.jpg" width="100%" alt="Result Image">';
+        scroll.innerHTML='<marquee><span id="color">For Dowload</span>:long press on the Result</marquee>';
+        var loginForm = document.getElementById('form');
+      loginForm.parentNode.removeChild(loginForm);
         break;
         case "22280-CM-013":
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0013.jpg" width="100%" alt="Result Image">';
+        scroll.innerHTML='<marquee><span id="color">For Dowload</span>:long press on the Result</marquee>';
+        var loginForm = document.getElementById('form');
+      loginForm.parentNode.removeChild(loginForm);
         break;
         case "22280-CM-014":
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0014.jpg" width="100%" alt="Result Image">';
+        scroll.innerHTML='<marquee><span id="color">For Dowload</span>:long press on the Result</marquee>';
+        var loginForm = document.getElementById('form');
+      loginForm.parentNode.removeChild(loginForm);
         break;
         case "22280-CM-015":
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0015.jpg" width="100%" alt="Result Image">';
+        scroll.innerHTML='<marquee><span id="color">For Dowload</span>:long press on the Result</marquee>';
+        var loginForm = document.getElementById('form');
+      loginForm.parentNode.removeChild(loginForm);
         break;
         case "22280-CM-016":
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0016.jpg" width="100%" alt="Result Image">';
+        scroll.innerHTML='<marquee><span id="color">For Dowload</span>:long press on the Result</marquee>';
+        var loginForm = document.getElementById('form');
+      loginForm.parentNode.removeChild(loginForm);
         break;
         case "22280-CM-017":
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0017.jpg" width="100%" alt="Result Image">';
+        scroll.innerHTML='<marquee><span id="color">For Dowload</span>:long press on the Result</marquee>';
+        var loginForm = document.getElementById('form');
+      loginForm.parentNode.removeChild(loginForm);
         break;
         case "22280-CM-018":
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0018.jpg" width="100%" alt="Result Image">';
+        scroll.innerHTML='<marquee><span id="color">For Dowload</span>:long press on the Result</marquee>';
+        var loginForm = document.getElementById('form');
+      loginForm.parentNode.removeChild(loginForm);
         break;
         case "22280-CM-019":
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0019.jpg" width="100%" alt="Result Image">';
+        scroll.innerHTML='<marquee><span id="color">For Dowload</span>:long press on the Result</marquee>';
+        var loginForm = document.getElementById('form');
+      loginForm.parentNode.removeChild(loginForm);
         break;
         case "22280-CM-020":
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0020.jpg" width="100%" alt="Result Image">';
+        scroll.innerHTML='<marquee><span id="color">For Dowload</span>:long press on the Result</marquee>';
+        var loginForm = document.getElementById('form');
+      loginForm.parentNode.removeChild(loginForm);
         break;
         case "22280-CM-021":
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0001.jpg" width="100%" alt="Result Image">';
+        scroll.innerHTML='<marquee><span id="color">For Dowload</span>:long press on the Result</marquee>';
+        var loginForm = document.getElementById('form');
+      loginForm.parentNode.removeChild(loginForm);
         break;
         case "22280-CM-022":
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0022.jpg" width="100%" alt="Result Image">';
+        scroll.innerHTML='<marquee><span id="color">For Dowload</span>:long press on the Result</marquee>';
+        var loginForm = document.getElementById('form');
+      loginForm.parentNode.removeChild(loginForm);
         break;
         case "22280-CM-023":
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0023.jpg" width="100%" alt="Result Image">';
+        scroll.innerHTML='<marquee><span id="color">For Dowload</span>:long press on the Result</marquee>';
+        var loginForm = document.getElementById('form');
+      loginForm.parentNode.removeChild(loginForm);
         break;
         case "22280-CM-024":
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0024.jpg" width="100%" alt="Result Image">';
+       scroll.innerHTML='<marquee><span id="color">For Dowload</span>:long press on the Result</marquee>';
+        var loginForm = document.getElementById('form');
+      loginForm.parentNode.removeChild(loginForm);
         break;
         break;
         case "22280-CM-026":
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0025.jpg" width="100%" alt="Result Image">';
+        scroll.innerHTML='<marquee><span id="color">For Dowload</span>:long press on the Result</marquee>';
+        var loginForm = document.getElementById('form');
+      loginForm.parentNode.removeChild(loginForm);
         break;
         case "22280-CM-027":
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0026.jpg" width="100%" alt="Result Image">';
+        scroll.innerHTML='<marquee><span id="color">For Dowload</span>:long press on the Result</marquee>';
+        var loginForm = document.getElementById('form');
+      loginForm.parentNode.removeChild(loginForm);
         break;
         case "22280-CM-028":
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0027.jpg" width="100%" alt="Result Image">';
+        scroll.innerHTML='<marquee><span id="color">For Dowload</span>:long press on the Result</marquee>';
+        var loginForm = document.getElementById('form');
+      loginForm.parentNode.removeChild(loginForm);
         break;
         case "22280-CM-029":
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0028.jpg" width="100%" alt="Result Image">';
+        scroll.innerHTML='<marquee><span id="color">For Dowload</span>:long press on the Result</marquee>';
+        var loginForm = document.getElementById('form');
+      loginForm.parentNode.removeChild(loginForm);
         break;
         case "22280-CM-030":
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0029.jpg" width="100%" alt="Result Image">';
+        scroll.innerHTML='<marquee><span id="color">For Dowload</span>:long press on the Result</marquee>';
+        var loginForm = document.getElementById('form');
+      loginForm.parentNode.removeChild(loginForm);
         break;
         case "22280-CM-031":
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0030.jpg" width="100%" alt="Result Image">';
+        scroll.innerHTML='<marquee><span id="color">For Dowload</span>:long press on the Result</marquee>';
+        var loginForm = document.getElementById('form');
+      loginForm.parentNode.removeChild(loginForm);
         break;
         case "22280-CM-032":
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0031.jpg" width="100%" alt="Result Image">';
+        scroll.innerHTML='<marquee><span id="color">For Dowload</span>:long press on the Result</marquee>';
+        var loginForm = document.getElementById('form');
+      loginForm.parentNode.removeChild(loginForm);
         break;
         case "22280-CM-033":
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0032.jpg" width="100%" alt="Result Image">';
+        scroll.innerHTML='<marquee><span id="color">For Dowload</span>:long press on the Result</marquee>';
+        var loginForm = document.getElementById('form');
+      loginForm.parentNode.removeChild(loginForm);
         break;
         case "22280-CM-034"||"22280-cm-034":
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0033.jpg" width="100%" alt="Result Image">';
+        scroll.innerHTML='<marquee><span id="color">For Dowload</span>:long press on the Result</marquee>';
+        var loginForm = document.getElementById('form');
+      loginForm.parentNode.removeChild(loginForm);
         break;
         
         case "22280-CM-035":
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0034.jpg" width="100%" alt="Result Image">';
+        scroll.innerHTML='<marquee><span id="color">For Dowload</span>:long press on the Result</marquee>';
+        var loginForm = document.getElementById('form');
+      loginForm.parentNode.removeChild(loginForm);
         break;
         case "22280-CM-036":
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0035.jpg" width="100%" alt="Result Image">';
+        scroll.innerHTML='<marquee><span id="color">For Dowload</span>:long press on the Result</marquee>';
+        var loginForm = document.getElementById('form');
+      loginForm.parentNode.removeChild(loginForm);
         break;
         case "22280-CM-037":
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0036.jpg" width="100%" alt="Result Image">';
+        scroll.innerHTML='<marquee><span id="color">For Dowload</span>:long press on the Result</marquee>';
+        var loginForm = document.getElementById('form');
+      loginForm.parentNode.removeChild(loginForm);
         break;
         case "22280-CM-038":
         imageContainer.innerHTML = '<img src="Dipoma1stresultspdf_page-0037.jpg" width="100%" alt="Result Image">';
